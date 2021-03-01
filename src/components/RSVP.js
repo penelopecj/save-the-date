6f51e06dcfdde91f3d13f5d6866696222ed5bfd3
Penelope Jungreis <penelope.jungreis@gmail.com>
2021-03-01 10:57:02 +0000

    g cal btn done

--- src/components/RSVP.js
+++ src/components/RSVP.js
@@ -14,8 +14,8 @@ function RSVP() {
       </section>
       <section className="title-banner">
         <h3>Please save the date</h3>
-        <h2 className="date open-sans orchid">Sunday 10<span className="superscript">th</span> July 2022</h2>
-        <a target="_blank" rel="noreferrer" href="https://calendar.google.com/event?action=TEMPLATE&amp;tmeid=M3ZzOTZuaXBudTQ5dXQwZWgybzBrZDU5c2ogcGVubnl6YWMyQG0&amp;tmsrc=pennyzac2%40gmail.com"><img border="0" src="https://www.google.com/calendar/images/ext/gc_button1_en-GB.gif" /></a>
+        <h2 className="date open-sans orchid">Sunday 10<span className="superscript big">th</span> July 2022</h2>
+        <a target="_blank" rel="noreferrer" href="https://calendar.google.com/event?action=TEMPLATE&amp;tmeid=M3ZzOTZuaXBudTQ5dXQwZWgybzBrZDU5c2ogcGVubnl6YWMyQG0&amp;tmsrc=pennyzac2%40gmail.com"><img border="0" src="https://www.google.com/calendar/images/ext/gc_button1_en-GB.gif" alt="Google calendar button" className="google-btn" /></a>
         <h4 className="fancy-font">More details coming soon.</h4>
       </section>
     </main>
